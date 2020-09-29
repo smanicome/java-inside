fdcd16a75e38e42b91d4743c560fbbb896795c20
smanicome <43649039+smanicome@users.noreply.github.com>
2020-09-29 10:16:20 +0200

    lab2

---
+++ lab2/src/main/java/Alien.java
@@ -0,0 +1,10 @@
+import static java.util.Objects.requireNonNull;
+
+public record Alien(int age, String planet) {
+    public Alien {
+        if (age < 0) {
+            throw new IllegalArgumentException("negative age");
+        }
+        requireNonNull(planet);
+    }
+}
